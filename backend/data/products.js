b2942380dd5333ec50fce8d7230c9e5306501419
ade475 <adeniji229@gmail.com>
2020-11-12 23:26:19 +0000

    revision

---
+++ backend/data/products.js
@@ -0,0 +1,77 @@
+const products = [
+  {
+    name: 'Citizen Promaster Titanium Men watch',
+    image: '/images/watch-3.jpg',
+    description:
+      'Citizen Promaster Titanium Dive Gmt BJ7111-51M is an amazing and attractive Gents watch from Promaster collection. Case material is Titanium. The watch is shipped with an original box and a guarantee from the manufacturer',
+
+    brand: 'Citizen',
+    category: 'luxurious timekeeping',
+    price: 3099.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Patek Philippe',
+    image: '/images/gold.jpg',
+    description:
+      'The watch is shipped with an original box and a guarantee from the manufacturere',
+    brand: 'Patek Philippe',
+    category: 'Luxury Watch',
+    price: 11599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Cartier',
+    image: '/images/rosegold.jpg',
+    description:
+      'Cartier is proud to present its Limited Edition Red Arrows Skyhawk A.T timepiece. Thanks to its Radio-Controlled Atomic Clock Synchronisation Technology. this watch keeps incredibly precise time. The RAF Red Arrows Limited Edition insignia is engraved on the case back whilst its official logo appears on the dial. Limited to 1/9999 Pieces Worldwide and presented in a Limited Edition Presentation Box. This watch is 200 metres water resistant, meaning it can be used for professional marine activity, diving and high impact water sports, but not deep sea diving.',
+    brand: 'Cartier',
+    category: 'Luxury Watch',
+    price: 35929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Armani Exchange',
+    image: '/images/blacksteel.jpg',
+    description:
+      'Armani Exchange Hampton AX2415 is a practical and handsome Gents watch from Smart collection. Case material is Stainless Steel while the dial colour is Gold. This model has got 50 metres water resistancy - it can be submerged in water for periods, so can be used for swimming and fishing. It is not recommended for high impact water sports. We ship it with an original box and a guarantee from the manufacturer',
+    brand: 'Armani',
+    category: 'Luxury Watch',
+    price: 1399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Tissot',
+    image: '/images/tissot.jpg',
+    description:
+      'Tissot is a practical and handsome Gents watch from Smart collection. Case material is Stainless Steel while the dial colour is Gold. This model has got 50 metres water resistancy - it can be submerged in water for periods, so can be used for swimming and fishing. It is not recommended for high impact water sports. We ship it with an original box and a guarantee from the manufacturer',
+    brand: 'Tissot',
+    category: 'Luxury Watch',
+    price: 4549.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Citizen Eco-Drive',
+    image: '/images/watch_4.jpg',
+    description:
+      'An amazing and attractive Gents watch from Eco-Drive collection. Case material is Titanium. The watch is shipped with an original box and a guarantee from the manufacturer',
+    brand: 'Citizen ',
+    category: 'Luxury Watch',
+    price: 829.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { config } from 'dotenv';
+import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('API is running...');
+});
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.get('/api/config/paypal', (req, res) =>
+	res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
+	)
+);
